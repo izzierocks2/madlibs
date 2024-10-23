86edf1419baaa10cb51e6967d03befd67287ae45
Grace <gracek2705@tuckahoeschools.org>
2024-10-23 12:30:30 +0000

    init commit

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+adjective1 = input("Type an adjective: ")
+animal1 = input("Type a noun: ")
+place1 = input("Type a place: ")
+food1 = input ("Type a food: ")
+game1 = input("Type a game: ")
+vacation1 = input("Type a vacation spot: ")
+animal2 = input("Type another animal: ")
+emotion1 = input("Type an emotion: ")
+sport1 = input("Type a sport: ")
+positive-adjective1 = input("Type an adjective: ")
+
+print(f"""Once upon a time there was a {adjective1} {animal1}. It lived at the {place1}. The {noun1} loves to eat {food1} and play {game1}.
+On a warm summer morning, the {noun1} left for the airport to go on vacation in {vacation1} with the {animal2}.
+The {animal1} was {emotion1} to be going on vacation. The {animal1} was excited to go to the {sport1} game when they arrived in {vacation1}. 
+The {animal1} and {animal2} arrived in {vacation1} and saw the {positive-adjective1} hotel they were staying at.
+After they checked into the hotel, the {animal1} and {animal2} went to the beach and went to play in the ocean.""")
